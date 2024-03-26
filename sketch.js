57e08fb1320a733d8bad62f25b8120cd81ec5a45
Heng <office.hchy@gmail.com>
2024-03-26 19:12:51 +0100

    added highlight effect.

--- sketch.js
+++ sketch.js
@@ -7,10 +7,21 @@ function setup() {
   left.size(windowWidth / 2, windowHeight);
   left.position(0, 0);
   left.style("background-color", "pink");
+  left.mouseOver(highlight);
+  left.mouseOut(unHighlight);
   right = createElement("div", "Video");
   right.size(windowWidth / 2, windowHeight);
   right.position(windowWidth / 2, 0);
   right.style("background-color", "blue");
+}
+
+function highlight() {
+  this.style("background-color", "white");
+}
+t;
+
+function unHighlight() {
+  this.style("background-color", "revert");
 }
 
 function draw() {
