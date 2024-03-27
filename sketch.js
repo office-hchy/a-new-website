17a4c7bc19338b377ae999af12e3d51cdea0d74e
Heng <office.hchy@gmail.com>
2024-03-27 11:56:28 +0100

    first commit

--- sketch.js
+++ sketch.js
@@ -1,28 +1,34 @@
 var left;
 var right;
+var officeName;
 
 function setup() {
   noCanvas();
-  left = createElement("div", "Drawing");
+  left = createElement("div");
+  left.class("block");
   left.size(windowWidth / 2, windowHeight);
   left.position(0, 0);
-  left.style("background-color", "pink");
+  //left.style("background-color", "pink");
   left.mouseOver(highlight);
   left.mouseOut(unHighlight);
-  right = createElement("div", "Video");
+  officeName = createP("ＨＣＨＹ");
+  //officeName.position(0, 0);
+  officeName.parent(left);
+  right = createElement("div");
+  right.class("block");
   right.size(windowWidth / 2, windowHeight);
   right.position(windowWidth / 2, 0);
-  right.style("background-color", "blue");
+  //right.style("background-color", "blue");
   right.mouseOver(highlight);
   right.mouseOut(unHighlight);
 }
 
 function highlight() {
-  this.style("background-color", "white");
+  this.style("background-color", "black");
 }
 
 function unHighlight() {
-  this.style("background-color", "pink");
+  this.style("background-color", "revert");
 }
 
 function draw() {
